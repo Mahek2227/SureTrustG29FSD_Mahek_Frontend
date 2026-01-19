bb363f38171fedd3e375623218537d0d29b071bc
Mahek2227 <maheksultana2004@gmail.com>
2026-01-19 19:18:36 +0530

    updated reset password

--- src/pages/Resetpassword.tsx
+++ src/pages/Resetpassword.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { baseUrl } from "../baseUrl";
 
-const sendOtpUrl = `${baseUrl}/otp/otp`;
-const changePasswordUrl = `${baseUrl}/otp/verify-otp`;
+const sendOtpUrl = `${baseUrl}/otp/send`;
+const changePasswordUrl = `${baseUrl}/otp/verify`;
 
 const ProfileCard: React.FC = () => {
   const [email, setEmail] = React.useState("");
